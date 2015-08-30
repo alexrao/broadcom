e16e10c80db64b922b80c428297a403f7a6996b1
alexrao <rxp2013@163.com>
2015-08-30 16:37:52 +0800

    添加串口相关设置（未完成 有事）

--- uart/.jhtty.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/mnt/hgfs/g_disk/cpu/src7231/nexus/800SH_AiJunGe/drv/jhtty.ko := mipsel-linux-ld -r  -m elf32ltsmip -T /home/WorkSpace/stblinux-2.6.37/scripts/module-common.lds --build-id  -o /mnt/hgfs/g_disk/cpu/src7231/nexus/800SH_AiJunGe/drv/jhtty.ko /mnt/hgfs/g_disk/cpu/src7231/nexus/800SH_AiJunGe/drv/jhtty.o /mnt/hgfs/g_disk/cpu/src7231/nexus/800SH_AiJunGe/drv/jhtty.mod.o
--- uart/.tmp_versions/jhtty.mod
+++
@@ -1,2 +0,0 @@
-/mnt/hgfs/g_disk/cpu/src7231/nexus/800SH_AiJunGe/drv/jhtty.ko
-/mnt/hgfs/g_disk/cpu/src7231/nexus/800SH_AiJunGe/drv/jhtty.o
